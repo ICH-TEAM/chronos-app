1f311a9a5854d3bbb2b744716d8fac7397cd8848
diegosalazaar <diego_2000_28@hotmail.com>
2022-07-19 01:01:11 -0500

    add login screens

---
+++ src/screens/LogIn/SignIn.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import Logo from './../../assets/svg/chronos-logo'
+import {Text, View, StyleSheet, TouchableOpacity} from 'react-native'
+import {Input, Button, Separator} from '../../components'
+const gap = 30
+export default function SignIn() {
+  return (
+    <View style={styles.container}>
+      <Logo />
+      <Separator value={5} />
+      <Text style={styles.title}>Iniciar Sesión</Text>
+      <View style={styles.form}>
+        <Input placeholder="Correo" />
+        <Separator value={gap} />
+        <Input placeholder="Contraseña" secureTextEntry />
+        <Separator value={gap} />
+        <Button label="Ingresar" />
+      </View>
+      <View style={styles.textLogIn}>
+        <Text style={styles.textLogInColor}>¿Aún no tienes una cuenta?</Text>
+        <TouchableOpacity>
+          <Text style={styles.textLogInColor}>Regístrate</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 80,
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    marginTop: 30,
+  },
+
+  stretch: {
+    width: 50,
+    height: 200,
+    resizeMode: 'stretch',
+  },
+  textLogIn: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  textLogInColor: {
+    color: '#797979',
+  },
+  form: {
+    marginTop: 30,
+    marginBottom: 100,
+  },
+})
